275524cf3ef32b68e6478348d9b9c3c32a17ca7c
Matilda Arvidsson <matilda.j.arvidsson@gmail.com>
2021-05-14 15:43:18 +0200

    test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import React from 'react'
 export const App = () => {
   return (
     <div>
-      Find me in src/app.js!
+      Matilda.
+      <iframe
+  src="https://technigo-test-nkjt.whereby.com/room?embed"
+  allow="camera; microphone; fullscreen; speaker; display-capture"
+></iframe>
     </div>
   )
 }
