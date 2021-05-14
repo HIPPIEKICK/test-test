1c60e2fd4e9e3e8091ef93f257cb4fb8f429ecb1
Matilda Arvidsson <matilda.j.arvidsson@gmail.com>
2021-05-14 17:11:47 +0200

    bigger

--- src/TestWhereby.js
+++ src/TestWhereby.js
@@ -5,6 +5,8 @@ import React from "react"
 export const TestWhereby = () => {
   return (
     <iframe
+      width="100%"
+      height="100%"
       src="https://technigo-test-nkjt.whereby.com/acd6119e-1912-4ceb-8c55-c66dd37796ad"
       allow="camera; microphone; fullscreen; speaker; display-capture"
     ></iframe>
