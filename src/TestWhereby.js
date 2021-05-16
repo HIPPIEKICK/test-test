250f7acddaae22e2972120be6e21639f4e442d9f
Matilda Arvidsson <matilda.j.arvidsson@gmail.com>
2021-05-16 11:43:10 +0200

    new url

--- src/TestWhereby.js
+++ src/TestWhereby.js
@@ -5,7 +5,7 @@ import React from "react"
 export const TestWhereby = () => {
   return (
     <iframe
-      src="https://technigo-test-nkjt.whereby.com/matildam7ng0y?embed?audio=off?screenshare=off"
+      src="https://technigo-test-nkjt.whereby.com/matildaw2dk7e?embed?audio=off?screenshare=off"
       allow="camera; microphone; fullscreen; speaker; display-capture"
     ></iframe>
 )
