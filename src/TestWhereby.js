90ff92dc1d5ac894f6cc07a3f9ac9a296e9a7403
Matilda Arvidsson <matilda.j.arvidsson@gmail.com>
2021-05-16 11:17:03 +0200

    new link

--- src/TestWhereby.js
+++ src/TestWhereby.js
@@ -6,8 +6,8 @@ export const TestWhereby = () => {
   return (
     <iframe
       width="100%"
-      height="100%"
-      src="https://technigo-test-nkjt.whereby.com/acd6119e-1912-4ceb-8c55-c66dd37796ad"
+      height="100vh"
+      src="https://technigo-test-nkjt.whereby.com/matildas0n6dp?embed?audio=off?screenshare=off"
       allow="camera; microphone; fullscreen; speaker; display-capture"
     ></iframe>
 )
