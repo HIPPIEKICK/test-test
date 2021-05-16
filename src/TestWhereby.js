3e1299513788757f16876c41d3ffc5b65ec01955
Matilda Arvidsson <matilda.j.arvidsson@gmail.com>
2021-05-16 11:18:28 +0200

    change height

--- src/TestWhereby.js
+++ src/TestWhereby.js
@@ -5,8 +5,6 @@ import React from "react"
 export const TestWhereby = () => {
   return (
     <iframe
-      width="100%"
-      height="100vh"
       src="https://technigo-test-nkjt.whereby.com/matildas0n6dp?embed?audio=off?screenshare=off"
       allow="camera; microphone; fullscreen; speaker; display-capture"
     ></iframe>
